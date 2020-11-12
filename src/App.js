1105438cd285a1dd770da153f5fefa9e0f5739ee
koftov <ahronlu@gmail.com>
2020-11-12 09:26:23 +0200

    darkMode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import FilmList from "./components/FilmList";
 import Header from "./components/Header";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <>
-      <Header/>
+    <div className={darkMode ? "inverse" : ""}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <FilmList />
-    </>
+    </div>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import { Menu, Icon } from "semantic-ui-react";
 
-const Header = ({setDarkMode,darkMode}) => {
-    return (
-        <Menu inverted={darkMode}>
-            <Menu.Item>Cinema</Menu.Item>
-            <Menu.Item
-                link
-                title="Dark/Light Mode"
-            >
-                <Icon name={darkMode ? "sun" : "moon"} onClick={() => setDarkMode(!darkMode)}  />
-            </Menu.Item>
-            <Menu.Item
-                position="right"
-                as="a"
-                href="https://github.com/koftov"
-                target="_blank"
-                title="My Github"
-            >
-                <Icon name="github" />
-            </Menu.Item>
-        </Menu>
-    )
-}
+const Header = ({ setDarkMode, darkMode }) => {
+  return (
+    <Menu inverted={darkMode}>
+      <Menu.Item>Cinema</Menu.Item>
+      <Menu.Item link title="Dark/Light Mode">
+        <Icon
+          name={darkMode ? "sun" : "moon"}
+          onClick={() => setDarkMode(!darkMode)}
+        />
+      </Menu.Item>
+      <Menu.Item
+        position="right"
+        as="a"
+        href="https://github.com/koftov"
+        target="_blank"
+        title="My Github"
+      >
+        <Icon name="github" />
+      </Menu.Item>
+    </Menu>
+  );
+};
 
-export default Header
+export default Header;
